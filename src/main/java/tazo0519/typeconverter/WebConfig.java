83d8dc30d3fe1ca2b3c70a38562ea25d3ae155ba
tazo0519 <tazo0519@naver.com>
2026-05-10 00:39:25 +0900

    섹션 11. 스프링 타입 컨버터 - 스프링에 Converter 적용

---
+++ src/main/java/tazo0519/typeconverter/WebConfig.java
@@ -0,0 +1,20 @@
+package tazo0519.typeconverter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import tazo0519.typeconverter.converter.IpPortToStringConverter;
+import tazo0519.typeconverter.converter.StringToIpPortConverter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        //주석처리 우선순위
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
