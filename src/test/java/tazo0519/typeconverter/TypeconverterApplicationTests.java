7df6ac847d2cd31c1114db07b8902948869478ec
tazo0519 <tazo0519@naver.com>
2026-05-09 23:34:15 +0900

    Initialize Project

---
+++ src/test/java/tazo0519/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package tazo0519.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
