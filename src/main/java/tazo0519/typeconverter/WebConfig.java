0498a1ab05da7e1f972fea915619ac81e4c6243f
tazo0519 <tazo0519@naver.com>
2026-05-10 13:18:31 +0900

    섹션 11. 스프링 타입 컨버터 - Formatter 적용 & 스프링이 제공하는 기본 포매터

--- src/main/java/tazo0519/typeconverter/WebConfig.java
+++ src/main/java/tazo0519/typeconverter/WebConfig.java
@@ -5,16 +5,19 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tazo0519.typeconverter.converter.IpPortToStringConverter;
 import tazo0519.typeconverter.converter.StringToIpPortConverter;
+import tazo0519.typeconverter.formatter.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        //주석처리 우선순위
+        // Converter가 Formatter보다 우선순위가 높아서 주석 처리
 //        registry.addConverter(new StringToIntegerConverter());
 //        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+        
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
